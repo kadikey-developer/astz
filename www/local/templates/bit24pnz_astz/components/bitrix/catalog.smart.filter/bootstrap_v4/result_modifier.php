81e1c6f9e10590edf0f78880d37333f0c8951879
kadikey-developer <kadikey58.developer@gmail.com>
2022-09-14 17:32:11 +0300

    Сортировка характеристики в фильтре --работает, но в начало подставляется в обратном порядке по коду сорт

--- www/local/templates/bit24pnz_astz/components/bitrix/catalog.smart.filter/bootstrap_v4/result_modifier.php
+++ www/local/templates/bit24pnz_astz/components/bitrix/catalog.smart.filter/bootstrap_v4/result_modifier.php
@@ -47,3 +47,45 @@ if ( !empty($arFilterSections) ) {
 		"~SMART_FILTER_PATH" => $arParams["~SMART_FILTER_PATH"],
 	);
 }
+
+/**
+ * Жёстко задаём сортировку для характеристик в фильтре
+ */
+$arFirstItems = array(
+	"MOSHCHNOST_VT",
+	"SVETOVOY_POTOK_LM",
+	"TSVETOVAYA_TEMPERATURA_K",
+	"INDEKS_TSVETOPEREDACHI",
+	"IP_STEPEN_ZASHCHITY",
+	"BAP_BLOK_AVARIYNOGO_PITANIYA",
+	"UPRAVLENIE",
+	"UGOL_RASSEYANIYA_",
+	"TIP_SVETOPROPUSKNOY_ARMATURY",
+	"TSVET_SVETOPROPUSKAYUSHCHEY_ARMATURY",
+);
+// $tempArray = array();
+/*foreach ($arFirstItems as $cur) { // найти в $arResult текущее значение $arFirstItems
+	
+	?><pre><?// print_r($cur); ?></pre><?
+	// $key = array_search("CODE" == $cur, $arResult["ITEMS"]);
+	$key = array_search($cur, array_column($arResult["ITEMS"], 'CODE'));
+	?><pre><?// print_r($key); ?></pre><?
+	// array_push(array_values()
+}*/
+
+/*foreach ($arResult["ITEMS"] as $key => $item) { // получаем список ключей которые есть
+	if ( in_array($item["CODE"], $arFirstItems) ) {
+		array_search($arFirstItems)
+	}
+}*/
+
+foreach ($arResult["ITEMS"] as $key => $item) { // Если в текущем $arResult есть значение из $arFirstItems
+	if ( in_array($item["CODE"], $arFirstItems) ) {
+		unset($tempArray);
+		unset($arResult["ITEMS"][$key]);
+		$arResult["ITEMS"] = array($key => $item) + $arResult["ITEMS"];
+	}
+} // подставляет их в обратном порядке в начало массива
+?><pre><?// print_r($tempArray); ?></pre><?
+
+?><pre><?// var_dump($arResult["ITEMS"]); ?></pre><?
