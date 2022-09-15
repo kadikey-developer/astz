4ecdfc4495ca0c0aa18eabc8a84a84aae62b1e04
kadikey-developer <kadikey58.developer@gmail.com>
2022-09-15 14:06:13 +0300

    Устранен баг с присвоением Meta Title --из-за отсутсвия элементов на странице не подставлялся шаблон.

---
+++ www/local/templates/bit24pnz_astz/components/bitrix/catalog.section.list/bootstrap_v4/component_epilog.php
@@ -0,0 +1,16 @@
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+/**
+ * @var array $arParams
+ * @var array $templateData
+ * @var string $templateFolder
+ * @var CatalogSectionComponent $component
+ */
+
+global $APPLICATION;
+
+/**
+ * Принудительно устанавливаем заголовок страницы.
+ * -- он перезаписывается, если в компоненте устанавливается другой, так что всё нормально.
+ */
+$APPLICATION->SetPageProperty("title", $arResult["SECTION"]["IPROPERTY_VALUES"]["SECTION_META_TITLE"]);
